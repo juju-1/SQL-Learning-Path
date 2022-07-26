921fbcc91d54096ffd514cce0f3d8f1e0aad2946
juju-1 <103675592+juju-1@users.noreply.github.com>
2022-07-25 21:39:37 -0500

    Add files via upload

---
+++ SQL/SQLQuery-union.sql
@@ -0,0 +1,43 @@
+/*
+UNION, UNION ALL
+*/
+
+Table 1 Insert:
+Insert into EmployeeDemographics VALUES
+(1011, 'Ryan', 'Howard', 26, 'Male'),
+(NULL, 'Holly', 'Flax', NULL, NULL),
+(1013, 'Darryl', 'Philbin', NULL, 'Male')
+
+Table 3 Query:
+Create Table WareHouseEmployeeDemographics 
+(EmployeeID int, 
+FirstName varchar(50), 
+LastName varchar(50), 
+Age int, 
+Gender varchar(50)
+)
+
+Table 3 Insert:
+Insert into WareHouseEmployeeDemographics VALUES
+(1013, 'Darryl', 'Philbin', NULL, 'Male'),
+(1050, 'Roy', 'Anderson', 31, 'Male'),
+(1051, 'Hidetoshi', 'Hasagawa', 40, 'Male'),
+(1052, 'Val', 'Johnson', 31, 'Female'
+
+SELECT *
+FROM EmployeeDemographics
+FULL OUTER JOIN WareHouseEmployeeDemographics
+	ON EmployeeDemographics.EmployeeID = WareHouseEmployeeDemographics.EmployeeID
+
+SELECT *
+FROM EmployeeDemographics
+UNION ALL
+SELECT*
+FROM WareHouseEmployeeDemographics
+
+
+SELECT EmployeeID, FirstName, Age
+FROM EmployeeDemographics
+UNION
+SELECT EmployeeID, JobTitle, Salary
+FROM EmployeeSalary

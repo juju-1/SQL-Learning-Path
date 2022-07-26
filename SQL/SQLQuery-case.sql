4f2a5d4bca1c972f37a854a48ffc9621b7624775
juju-1 <103675592+juju-1@users.noreply.github.com>
2022-07-26 10:40:07 -0500

    Add files via upload

---
+++ SQL/SQLQuery-case.sql
@@ -0,0 +1,26 @@
+/*
+Case Statement
+*/
+
+SELECT FirstName, LastName, Age, --(',' NEED ADD BEFORE CASE)
+CASE
+	WHEN Age = 38 THEN 'Stanley' -- case statement run in order
+	WHEN Age > 30 THEN 'Old'
+	WHEN Age BETWEEN 27 AND 30 THEN 'Young'
+	ELSE 'Baby'
+END
+FROM EmployeeDemographics
+WHERE Age is not null
+ORDER BY 3
+
+
+SELECT FirstName, LastName, JobTitle, Salary,
+CASE
+	WHEN JobTitle = 'Salesman' THEN Salary + (Salary * .10)
+	WHEN JobTitle = 'Accountant' THEN Salary + (Salary * .05)
+	WHEN JobTitle = 'HR' THEN Salary + (Salary * .0001)
+	ELSE Salary + (Salary * .03)
+END AS SalaryAfterRaise
+FROM EmployeeDemographics
+JOIN EmployeeSalary
+	ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID

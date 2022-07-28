632f81fe35a526e0f6777f4627358732dbe812c9
JuJu <103675592+juju-1@users.noreply.github.com>
2022-07-27 19:01:47 -0500

    Update 15-SQLQuery-procedure.sql

--- SQL/15-SQLQuery-procedure.sql
+++ SQL/15-SQLQuery-procedure.sql
@@ -33,6 +33,34 @@ FROM #TEM_TRY
 
 EXEC TRY_1
 
--- after modify in procedure file add parameters @JobTitle
 
-EXEC TRY_1 @JobTitle = 'Salesman'
+-- after modify in procedure file add parameters @JobTitle
+CREATE PROCEDURE try1
+@JobTitle nvarchar(100)
+AS
+DROP TABLE IF EXISTS try1
+Create table #try1 (
+JobTitle varchar(100),
+EmployeesPerJob int ,
+AvgAge int,
+AvgSalary int
+)
+
+
+Insert into #try1
+SELECT JobTitle, Count(JobTitle), Avg(Age), AVG(salary)
+FROM SQLTutorial..EmployeeDemographics emp
+JOIN SQLTutorial..EmployeeSalary sal
+	ON emp.EmployeeID = sal.EmployeeID
+where JobTitle = @JobTitle --- make sure to change this in this script from original above
+group by JobTitle
+
+Select * 
+From #try1
+GO;
+
+
+exec try1 @jobtitle = 'Salesman'
+exec try1 @jobtitle = 'Accountant'
+
+
